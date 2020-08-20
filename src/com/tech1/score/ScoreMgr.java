08828f5673f51918d1eda795314670c128c16b7d
beatcorea <beatcorea@gmail.com>
2020-08-20 10:50:23 +0900

    score_step1

--- src/com/tech1/score/ScoreMgr.java
+++ src/com/tech1/score/ScoreMgr.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class ScoreMgr {
 	public static void main(String[] args) {
-		do {
 			Scanner sc=new Scanner(System.in);
 			int sum=0;
 			int max=0;
@@ -12,10 +11,6 @@ public class ScoreMgr {
 			System.out.println("5개의 점수를 입력하세요");
 			for (int i = 0; i < scrArr.length; i++) {
 				scrArr[i]=sc.nextInt();
-				if (scrArr[i]<50 || scrArr[i]>101) {
-					System.out.println("점수를 잘 못 입력하였습니다");
-					break;
-				}
 			}
 			for (int i = 0; i < scrArr.length; i++) {
 				sum+=scrArr[i];
@@ -24,18 +19,5 @@ public class ScoreMgr {
 				}
 			}
 			System.out.println("평균 : "+(sum/5));
-			scrArr[0]=0;
-			System.out.println("프로그램을 종료하시겠습니까?\n(-1:프로그램종료, 1:프로그램재시작)");
-			
-			Scanner sr=new Scanner(System.in);
-			int a=sr.nextInt();
-				if (a==-1) {
-					System.out.println("프로그램을 종료합니다");
-					break;
-				}else if (a==1) {
-					continue;
-				}
-			}while(true);
-		return;
 	}
 }
